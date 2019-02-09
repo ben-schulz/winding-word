de2670de5b513ce27662a3587c9579029a88af91
ben-schulz <benjamin.john.schulz@gmail.com>
2019-02-09 11:47:11 -0600

    refactor(project): remove unused.
    remove placeholders.

--- src/placeholder.js
+++
@@ -1,7 +0,0 @@
-class Foo{
-
-    constructor(){
-
-	this.check = 'ok';
-    }
-}
--- tests/placeholder.js
+++
@@ -1,9 +0,0 @@
-describe( "the placeholder", function(){
-
-    it( "is where it should be", function(){
-
-	var result = new Foo();
-	
-	assert.equal( 'ok', result.check );
-    } );
-} );
