d158c531ae6aa27f7597f65cd77e1821b049dab5
ben-schulz <benjamin.john.schulz@gmail.com>
2019-02-23 14:04:23 -0600

    feat(input): bind keys in app.
    add cursor actions to
    main application.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,64 @@
+var keyMap = {
+
+    "w": "moveUp",
+    "s": "moveDown",
+    "a": "moveLeft",
+    "d": "moveRight",
+};
+
+var bindHandlers = function( page ){
+
+    return {
+	"moveUp": page.cursorUp,
+	"moveDown": page.cursorDown,
+	"moveLeft": page.cursorLeft,
+	"moveRight": page.cursorRight,
+    };
+};
+
+var bindKeys = function( handlers ){
+
+    var result = {};
+    Object.keys( keyMap ).forEach( key => {
+
+	var actionName = keyMap[ key ];
+
+	result[ key ] = handlers[ actionName ];
+    } );
+
+    return result;
+};
+
+
+var bindKeyboardEvents = function( handlers ){
+
+    document.addEventListener(
+
+	"keydown", event => {
+
+	    var key = event.key;
+
+	    if( key in handlers ){
+		handlers[ key ]();
+	    }
+	}
+    );
+};
+
 var textLoader = new TextLoader();
 
 document.body.appendChild( textLoader.element );
 
 textLoader.onload = text => {
 
-    var textNode = new TextPage( text )
+    var page = new TextPage( text );
 
-    document.body.appendChild( textNode.element );
+    var pageHandlers = bindHandlers( page );
+    var keyHandlers = bindKeys( pageHandlers );
+
+    bindKeyboardEvents( keyHandlers );
+
+    document.body.appendChild( page.element );
 };
 
 
